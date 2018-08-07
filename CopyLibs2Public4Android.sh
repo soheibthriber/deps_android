63556e64c4ad931260f265c6a30bbe297497a88b
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-08-07 15:28:58 +0200

    moved real3d.jar

--- CopyLibs2Public4Android.sh
+++ CopyLibs2Public4Android.sh
@@ -6,4 +6,4 @@ cp -r lib/android/armeabi-v7a/*.so ../gpac_public/extra_lib/lib/android/armeabi-
 mkdir -p ../gpac_public/extra_lib/lib/android/x86
 cp -r lib/android/x86/*.so ../gpac_public/extra_lib/lib/android/x86
 mkdir -p ../gpac_public/applications/osmo4_android/libs
-cp -f ../tools/osmo4_android_libs/real3d.jar ../gpac_public/applications/osmo4_android/libs
+cp -f ./real3d.jar ../gpac_public/applications/osmo4_android/libs
